24909559e8c652b93deaae473d9c5ddd6a84dd63
Justin Rhinesmith <jerhinesmith@gmail.com>
2016-12-07 15:53:10 -0800

    Try jruby

--- Gemfile
+++ Gemfile
@@ -1,12 +1,13 @@
 source 'https://rubygems.org'
 
-ruby '2.3.3'
+# ruby '2.3.3'
+ruby '2.3.1', engine: 'jruby', engine_version: '9.1.5.0'
 
 # Server
 gem 'sinatra'
 
 # Image generation
-gem 'rmagick'
+gem 'mini_magick'
 
 # Monitoring
 gem 'newrelic_rpm'
--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'newrelic_rpm'
-require 'RMagick'
 require 'digest/sha1'
+require 'mini_magick'
 
 get '/:color/:size/:text.png' do
   color, text, size = params['color'].downcase, params['text'].upcase[0, 2], params['size'].to_i
@@ -9,18 +9,19 @@ get '/:color/:size/:text.png' do
   # Hex codes
   color = "##{color}" if color.length == 6 && color =~ /\A[a-f0-9]+\z/
 
-  canvas = Magick::Image.new(size, size){ self.background_color = color }
-  canvas.format = 'png'
-  gc = Magick::Draw.new
-  gc.pointsize = size / 2
-  gc.font = File.join(File.dirname(__FILE__), 'lib', 'fonts', 'Helvetica.ttf')
-  gc.gravity = Magick::CenterGravity
-  gc.annotate(canvas, 0,0,0,0, text) {
-    self.fill = 'white'
-  }
+  image = MiniMagick::Tool::Convert.new do |convert|
+    convert.merge! ['-background', color]
+    convert.merge! ['-fill', 'white']
+    convert.merge! ['-font', File.join(File.dirname(__FILE__), 'lib', 'fonts', 'Helvetica.ttf')]
+    convert.merge! ['-size', "#{size}x#{size}"]
+    convert.merge! ['-pointsize', size / 2]
+    convert.merge! ['-gravity', 'center']
+    convert << "label:#{text}"
+    convert << "png:-"
+  end
 
-  content_type 'image/png'
   cache_control :public, :must_revalidate, max_age: (365 * 24 * 60 * 60)
   etag Digest::SHA1.hexdigest "#{text}#{color}#{size}"
-  canvas.to_blob
+  content_type 'image/png'
+  image
 end
